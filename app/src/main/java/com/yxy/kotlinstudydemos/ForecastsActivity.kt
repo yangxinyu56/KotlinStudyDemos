bdae7111b3901a9931ac79e0299044476508d7aa
yangxinyu <xinyu.yang@21chinamall.com>
2017-06-23 00:09:22 +0800

    gradle更新anko库版本

--- app/src/main/java/com/yxy/kotlinstudydemos/ForecastsActivity.kt
+++ app/src/main/java/com/yxy/kotlinstudydemos/ForecastsActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.yxy.kotlinstudydemos.POJO.Forecast
 import com.yxy.kotlinstudydemos.adapter.ItemClickSupport
 import com.yxy.kotlinstudydemos.adapter.MyDecoration
 import com.yxy.kotlinstudydemos.adapter.SimplerItemAdapter
 import com.yxy.kotlinstudydemos.net.ForecastRequest
-import org.jetbrains.anko.async
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.activity_forecasts.*
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class ForecastsActivity : AppCompatActivity() {
@@ -20,19 +19,19 @@ class ForecastsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecasts)
-        val rlvWXJP = find<RecyclerView>(R.id.rlv_weixinjp)
-        rlvWXJP.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        //val rlvWXJP = find<RecyclerView>(R.id.rlv_weixinjp)
+        rlv_weixinjp.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         var items: List<Forecast>
-        async {
+        doAsync {
             //异步执行发起请求
             val result = ForecastRequest(appKey).execute()
             uiThread {
                 //请求完毕之后线程切换
                 //Log.e("123456", result.result.toString())
                 items = result.result.list
-                rlvWXJP.adapter = SimplerItemAdapter(items, this@ForecastsActivity)
-                rlvWXJP.addItemDecoration(MyDecoration(this@ForecastsActivity, MyDecoration.VERTICAL_LIST))
-                ItemClickSupport.addTo(rlvWXJP).setOnItemClickListener { _, position, _ ->
+                rlv_weixinjp.adapter = SimplerItemAdapter(items, this@ForecastsActivity)
+                rlv_weixinjp.addItemDecoration(MyDecoration(this@ForecastsActivity, MyDecoration.VERTICAL_LIST))
+                ItemClickSupport.addTo(rlv_weixinjp).setOnItemClickListener { _, position, _ ->
                     val ine = Intent(this@ForecastsActivity, ForecastDetailActivity::class.java)
                     val b = Bundle()
                     b.putString("WEBVIEW_URL_KEY", items[position].url)
